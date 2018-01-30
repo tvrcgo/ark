29a1e584f50a5d2fc4884fc4947e7d4cde6fb3e0
tvrcgo <tvrcgo@gmail.com>
2018-01-30 12:05:31 +0800

    support less.

--- example/client/theme/index.js
+++ example/client/theme/index.js
@@ -1,7 +1,10 @@
 
+import './index.less'
+
 export default ({ app, children }) => {
   return (
-    <div>
+    <div className='app'>
+      <div className='app-header'>header</div>
       <h3>hello app</h3>
       <div>{children}</div>
     </div>
