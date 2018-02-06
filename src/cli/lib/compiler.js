2fa8ee44946a8ba599bb384792a189371b9ea105
tvrcgo <tvrcgo@gmail.com>
2018-02-06 13:53:13 +0800

    building tips.

--- src/cli/lib/compiler.js
+++ src/cli/lib/compiler.js
@@ -1,55 +1,46 @@
 
 const webpack = require('webpack')
-const emoji = require('node-emoji')
 const chalk = require('chalk')
+const ora = require('ora')
 
 const handler = (err, stats) => {
   const json = stats.toJson()
   if (err || stats.hasErrors()) {
     return json.errors.map(e => {
-      console.error(emoji.get('x'), e)
+      console.error(e)
     })
   }
   if (stats.hasWarnings()) {
     return json.warnings.map(warn => {
-      console.warn(emoji.get('warning'), warn)
+      console.warn(warn)
     })
   }
-  console.log(stats.toString({
+  console.log('\n', stats.toString({
     chunks: false,
     colors: true,
     modules: false
-  }))
+  }), '\n')
 }
 
 module.exports = (config) => {
 
+  const spin = ora()
+
+  config.plugins.push(new webpack.ProgressPlugin((percent, msg) => {
+    const perc = chalk.green(`${(percent*100).toFixed(1)}%`)
+    spin.text = `${perc} ${msg} ...`
+  }))
+
   const compiler = webpack(config)
 
   compiler.plugin("compile", (params) => {
-    process.stdout.write(emoji.emojify('\n:airplane: '))
-  })
-
-  compiler.plugin("compilation", (compilation) => {
-    let tm
-
-    compilation.plugin('build-module', (module) => {
-      tm = +new Date()
-    })
-
-    compilation.plugin('succeed-module', (module) => {
-      const cost = +new Date() - tm
-      const dot = cost < 1000 ? chalk.green('.') : ( cost < 3000 ? chalk.yellow('.') : chalk.magenta('.'))
-      process.stdout.write(dot)
-    })
-
-    compilation.plugin('failed-module', (module) => {
-      process.stdout.write(chalk.red('.'))
-    })
+    spin.text = 'Building start.'
+    spin.start()
   })
 
   compiler.plugin('done', (stats) => {
-    process.stdout.write(emoji.emojify(' :ok_hand:\n\n'))
+    spin.succeed('Building finished.')
+    spin.stop()
   })
 
   return {
