80b989f4b84486cea2e0155e30b290f17398c3c3
tvrcgo <tvrcgo@gmail.com>
2018-03-27 21:16:39 +0800

    optimize hotwatch

--- src/cli/lib/hotwatch.js
+++ src/cli/lib/hotwatch.js
@@ -1,40 +1,39 @@
 const http = require('http')
 
-function HotWatch(options = {}) {
+module.exports = class HotWatch {
 
-  let interval
-  let lastSignal = null
-  this.signal = null
+  constructor(options = {}) {
+    let interval
+    let lastSignal = null
+    this.signal = null
 
-  http.createServer((req, res) => {
-    res.writeHead(200, {
-      "Content-Type":"text/event-stream",
-      "Cache-Control":"no-cache",
-      "Connection":"keep-alive",
-      "Access-Control-Allow-Origin": '*'
+    http.createServer((req, res) => {
+      res.writeHead(200, {
+        "Content-Type":"text/event-stream",
+        "Cache-Control":"no-cache",
+        "Connection":"keep-alive",
+        "Access-Control-Allow-Origin": '*'
+      })
+      res.write(`retry: ${options.retry || 3000}\n`)
+
+      interval = setInterval(() => {
+        if (lastSignal != this.signal) {
+          res.write(`data: ${this.signal}\n\n`)
+          lastSignal = this.signal
+        }
+      }, 1000)
+
+      req.connection.addListener("close", function () {
+        clearInterval(interval)
+      }, false)
+
+    }).listen(32651)
+    console.log('[HMR] Hotwatch start ...', '\n')
+  }
+
+  apply(compiler) {
+    compiler.plugin('done', (stats) => {
+      this.signal = `done#${stats.hash}`
     })
-    res.write(`retry: ${options.retry || 3000}\n`)
-
-    interval = setInterval(() => {
-      if (lastSignal != this.signal) {
-        res.write(`data: ${this.signal}\n\n`)
-        lastSignal = this.signal
-      }
-    }, 1000)
-
-    req.connection.addListener("close", function () {
-      clearInterval(interval)
-    }, false)
-
-  }).listen(32651)
-  console.log('[HMR] Hotwatch start ...', '\n')
+  }
 }
-
-HotWatch.prototype.apply = function(compiler) {
-  // done
-  compiler.plugin('done', (stats) => {
-    this.signal = `done#${stats.hash}`
-  })
-}
-
-module.exports = HotWatch
